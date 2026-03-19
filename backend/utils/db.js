ab706c69d5f40319090223eef83448c67eb06c9c
sharfealam900 <sharfealam900@gmail.com>
2026-03-20 00:43:49 +0530

    debug mongo uri

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
-const connectDB=async()=>{
+
+const connectDB = async () => {
     try {
+        console.log("MONGO_URI:", process.env.MONGO_URI); 
+
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully')
-        } catch (error) {
-        console.log(error)
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.log(error);
     }
 }
+
 export default connectDB;
